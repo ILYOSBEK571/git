d10988bd1165edba411d86dc29285c024a543308
ODILOV.ILYOSBEK <ODILOV.ILYOSBEK571@gmail.com>
2021-08-12 21:38:40 +0500

    first commit, added

---
+++ jquery.js
@@ -0,0 +1,136 @@
+// let name1 = "ILYOSBEK";
+// console.log(name1);
+// name1 = "boshqa";
+// console.log(name1);
+
+// const lname = "ILYOSBEK";
+// console.log(lname);
+
+// var lname1 = "A";
+// console.log(lname1);
+
+// const street = "ILYOSBEK";
+// const number = 15;
+// const age = null;
+
+// console.log(typeof street);
+
+// const sring = "person st";
+// const numbeer = 157;
+// const tennt = "JON DOE";
+// console.log( `${sring} lives ${numbeer} ${tennt}`);
+
+// console.log(tennt.toLowerCase())
+// console.log(tennt.split(' '));
+
+
+// const aree = ["AUDI", "BMW", "TESLA"]
+// console.log(aree);
+
+// aree .push("toyota");
+// console.log(aree);
+
+// const persoon = {
+//      name2:"ILYOSBEK",
+//      yosh:18,
+
+//      adderss:{
+//      student:true,
+//      nomer:47
+//      }
+// }
+
+//  const{ name2, yosh, adderss: {student}, adderss: {nomer}} = persoon;
+//  console.log( name2, yosh, student, nomer );
+
+// console.log(JSON.stringify(persoon));
+
+
+// const students = [
+//   {
+//      fname:"John Doe",
+//      age:22,
+//      graduated:false
+//   },
+
+//    {
+//      fname:"Bob mork",
+//      age:21,
+//      graduated:true
+//   },
+
+//     {
+//      fname:"John smite",
+//      age:20,
+//      graduated:true
+//    }
+// ];
+
+// const graduated = students.filter(function(student){
+//      return student.graduated === true;
+// }).map(function(s){
+//      return s.fname
+// })
+// console.log(graduated);
+// console.log(students);
+
+
+
+
+
+
+
+// const score = 99;
+
+// if (score >=90){
+//      console.log ("A");
+// }
+//   else if (score < 90 && score >= 80){
+//   console.log("B")
+// }
+// else if (score < 80 && score >= 70){
+// console.log("C")
+// }
+// else if (score < 70 && score >= 60){
+//  console.log("D")
+// }
+// else{
+//      console.log("false");
+// }
+
+// const ball = 57;
+// if ( ball > 50 && ball < 90){
+//      console.log("true")
+// }
+// else{
+//      console.log("false")
+// }
+
+
+
+// function greet (time){
+//      switch (time){
+//           case "salom":
+//           console.log("salom");
+//           break;
+//           case "asalom":
+//                console.log("Assalomu alaykum");
+//                break;
+//                case "valeykum":
+//                     console.log("valekum assalom");
+//                     break;
+//      }
+// }
+
+// greet ("asalom")
+
+
+
+// function adde (num1, num2){
+//      console.log(num1 + num2);
+// }
+
+// adde(100,19)
+
+     // var bleep = new Audio();
+     // bleep.src = "Button Beep Sound Effect - Version 1 - HQ Sounds.wav";
